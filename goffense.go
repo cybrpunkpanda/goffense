49eca2cc131168e2b7fe1c637934757150515c6a
Otacon Stark <bqeye17@gmail.com>
2024-01-17 10:08:25 -0500

    Tested some issues with breakpoints

--- goffense.go
+++ goffense.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	//"regexp/syntax"
-	//"time"
-	//"os"
-	//"errors"
-	//"log"
-	//"flag"
-	//"github.com/rclone/rclone/backend/smb"
-	//"github.com/miekg/dns"
-)
+//"regexp/syntax"
+//"time"
+//"os"
+//"errors"
+//"log"
+//"flag"
+//"github.com/rclone/rclone/backend/smb"
+//"github.com/miekg/dns"
+
+func scanSMB(subnet string) {
 
 func scanSMB() {
 	fmt.Println("Hello World!")
 }
 
 func main() {
-	scanSMB()
+	
 }
