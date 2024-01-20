35b61318746bcfd005db8e37eea3f10e62e75b7a
Otacon Stark <bqeye17@gmail.com>
2024-01-20 14:54:02 -0500

    working with logic of user input

--- goffense.go
+++ goffense.go
@@ -2,21 +2,37 @@ package main
 
 import (
 	"fmt"
-//"regexp/syntax"
-//"time"
-//"os"
-//"errors"
-//"log"
-//"flag"
-//"github.com/rclone/rclone/backend/smb"
-//"github.com/miekg/dns"
+	"net"
+	"strings"
 
-func scanSMB(subnet string) {
+	//"regexp/syntax"
+	//"time"
+	//"os"
+	//"errors"
+	//"log"
+	"flag"
+	// "github.com/rclone/rclone/backend/smb"
+	// "github.com/miekg/dns"
+)
 
-func scanSMB() {
-	fmt.Println("Hello World!")
+var ip = flag.String("host", "123.123.123.123", "Use either a single IP or CIDR notation")
+
+func parseHostType(hostType string) {
+	if strings.Contains(*ip, "/") {
+		fmt.Println("This is a CIDR Notation")
+	} else if net.ParseIP(*ip) == nil {
+		fmt.Println("The input is not valid")
+	} else {
+		fmt.Println("This must be a single IP")
+	}
+}
+
+func scanSMB(ipAddr string) {
+
 }
 
 func main() {
-	
+	flag.Parse()
+
+	parseHostType(*ip)
 }
