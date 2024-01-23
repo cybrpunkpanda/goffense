722ea2fb978ed46f4b160f43e44d7adcdc2c7d4d
Otacon Stark <bqeye17@gmail.com>
2024-01-23 13:40:51 -0500

    progress on parsing host type

--- goffense.go
+++ goffense.go
@@ -16,23 +16,23 @@ import (
 )
 
 var ip = flag.String("host", "123.123.123.123", "Use either a single IP or CIDR notation")
+var file = flag.String("file", "filename", "Use a file with IP addresses by line")
 
 func parseHostType(hostType string) {
 	if strings.Contains(*ip, "/") {
 		fmt.Println("This is a CIDR Notation")
-	} else if net.ParseIP(*ip) == nil {
-		fmt.Println("The input is not valid")
-	} else {
+	} else if net.ParseIP(*ip) != nil {
 		fmt.Println("This must be a single IP")
+	} else {
+		fmt.Println("The input is not valid")
 	}
 }
 
-func scanSMB(ipAddr string) {
+func scanHost(ipAddr string) {
 
 }
 
 func main() {
 	flag.Parse()
 
-	parseHostType(*ip)
 }
